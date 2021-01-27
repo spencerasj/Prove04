53470808f34361b269aa798af5c8f3f87619606b
connorm3808 <connor3808@gmail.com>
2021-01-26 20:00:35 -0800

    Saying hello to jennifer

--- src/spencer/Main.java
+++ src/spencer/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("Hello, my name is Jennifer");
+	System.out.println("Hello Jennifer, my name is Connor");
     }
 }
