cfbc2cbc1286d37283f6f451dd5e9c51bf95cd7d
Jeremy Petersen <jeremy@comp-nerds.com>
2021-01-27 18:50:28 -0800

    Adding hello response

--- src/spencer/Main.java
+++ src/spencer/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("Hello, my name is Jennifer");
 	System.out.println("Hello Jennifer, my name is Connor");
+	System.out.println("Hello Jennifer, my name is Jeremy");
     }
 }
